14cf389d1629f4ef2f1782d0351fcbd5c6317634
dessa995 <dessa995@outlook.com>
2023-11-21 12:41:38 +0100

    deleted else branch

--- src/pages/MembersPage.jsx
+++ src/pages/MembersPage.jsx
@@ -27,55 +27,55 @@ const MembersPage = () => {
         </div>
       </PageLayout>
     );
-  } else {
-    return (
-      <PageLayout isLoading={isLoading}>
-        <div className="flex flex-col items-center px-20">
-          {members?.map((member, index) => (
-            <React.Fragment key={member?.createdAt}>
-              {index !== 0 && <Separator className="flex w-full h-10 my-10" />}
-              <div className="flex flex-col justify-center tems-stretch border border-maltYellow rounded-3xl mt-5  bg-guinessBlack w-[80%] hover:scale-105 hover:cursor-pointer transition-all duration-[500ms]">
-                <div className="flex gap-2">
-                  <div
-                    className={`flex w-[200px] mr-[50px] min-w-[200px] min-h-[200px] items-center relative overflow-hidden rounded-l-3xl`}
-                  >
-                    <img
-                      alt={`${member?.name} logo`}
-                      src={member?.logo}
-                      className="absolute min-w-[1000%] min-h-[1000%] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] scale-[0.1001]"
-                    />
+  }
+
+  return (
+    <PageLayout isLoading={isLoading}>
+      <div className="flex flex-col items-center px-20">
+        {members?.map((member, index) => (
+          <React.Fragment key={member?.createdAt}>
+            {index !== 0 && <Separator className="flex w-full h-10 my-10" />}
+            <div className="flex flex-col justify-center tems-stretch border border-maltYellow rounded-3xl mt-5  bg-guinessBlack w-[80%] hover:scale-105 hover:cursor-pointer transition-all duration-[500ms]">
+              <div className="flex gap-2">
+                <div
+                  className={`flex w-[200px] mr-[50px] min-w-[200px] min-h-[200px] items-center relative overflow-hidden rounded-l-3xl`}
+                >
+                  <img
+                    alt={`${member?.name} logo`}
+                    src={member?.logo}
+                    className="absolute min-w-[1000%] min-h-[1000%] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] scale-[0.1001]"
+                  />
+                </div>
+                <div className="flex flex-col align-start justify-between py-[20px]">
+                  <div>
+                    <Text className="mb-[15px]" size={'large'} color={'maltYellow'} text={member?.name} />
+                    <Text size="medium" color="white" text={member?.description} />
                   </div>
-                  <div className="flex flex-col align-start justify-between py-[20px]">
-                    <div>
-                      <Text className="mb-[15px]" size={'large'} color={'maltYellow'} text={member?.name} />
-                      <Text size="medium" color="white" text={member?.description} />
+                  <div className="flex gap-[25px] mt-4">
+                    <div className="flex min-h-[2.5rem] items-center">
+                      <a href={member.facebook} rel="noreferrer" target="_blank">
+                        <FacebookIcon className="w-8 h-8 cursor-pointer hover:w-10 hover:h-10 transition-all duration-[350ms]" />
+                      </a>
                     </div>
-                    <div className="flex gap-[25px] mt-4">
-                      <div className="flex min-h-[2.5rem] items-center">
-                        <a href={member.facebook} rel="noreferrer" target="_blank">
-                          <FacebookIcon className="w-8 h-8 cursor-pointer hover:w-10 hover:h-10 transition-all duration-[350ms]" />
-                        </a>
-                      </div>
-                      <div className="flex min-h-[2.5rem] items-center">
-                        <a href={member.instagram} rel="noreferrer" target="_blank">
-                          <InstagramIcon className="w-8 h-8 cursor-pointer hover:w-10 hover:h-10 transition-all duration-[350ms]" />
-                        </a>
-                      </div>
-                      <div className="flex min-h-[2.5rem] items-center">
-                        <a href={`mailto:${member.email}`} rel="noreferrer" target="_blank">
-                          <EmailIcon className="w-8 h-8 cursor-pointer hover:w-10 hover:h-10 transition-all duration-[350ms]" />
-                        </a>
-                      </div>
+                    <div className="flex min-h-[2.5rem] items-center">
+                      <a href={member.instagram} rel="noreferrer" target="_blank">
+                        <InstagramIcon className="w-8 h-8 cursor-pointer hover:w-10 hover:h-10 transition-all duration-[350ms]" />
+                      </a>
+                    </div>
+                    <div className="flex min-h-[2.5rem] items-center">
+                      <a href={`mailto:${member.email}`} rel="noreferrer" target="_blank">
+                        <EmailIcon className="w-8 h-8 cursor-pointer hover:w-10 hover:h-10 transition-all duration-[350ms]" />
+                      </a>
                     </div>
                   </div>
                 </div>
               </div>
-            </React.Fragment>
-          ))}
-        </div>
-      </PageLayout>
-    );
-  }
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    </PageLayout>
+  );
 };
 
 export default MembersPage;
