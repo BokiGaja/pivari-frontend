d0d31de2e1e04581797eb193a89226af40fa6d57
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2023-11-22 12:51:59 +0100

    Sponsors Page (#4)
    * initial commit sponsors
    
    * style change
    
    * responsive design

--- src/pages/SponsorsPage.jsx
+++ src/pages/SponsorsPage.jsx
@@ -3,6 +3,12 @@ import PageLayout from '../components/layout/PageLayout';
 import Text from '../components/Text/Text';
 import { useGetCollection } from '../services/api/hooks/useGetCollection';
 import { sanitizeResponseData } from '../utils/api/responseData';
+import { Typography } from '@mui/material';
+
+import { ReactComponent as FacebookIcon } from '../assets/svg/socialIcons/icon-facebook.svg';
+import { ReactComponent as InstagramIcon } from '../assets/svg/socialIcons/icon-instagram.svg';
+import { ReactComponent as EmailIcon } from '../assets/svg/socialIcons/icon-email.svg';
+import { ReactComponent as Separator } from '../assets/svg/separator.svg';
 
 const SponsorsPage = () => {
   const { data: sponsorsData, isLoading } = useGetCollection('sponsors');
@@ -10,12 +16,68 @@ const SponsorsPage = () => {
     ...sponsor.attributes,
     logo: sanitizeResponseData(sponsor.attributes, 'logo')?.url,
   }));
-  console.log('sponsorsData', sponsors);
+
+  if (!sponsors?.length) {
+    return (
+      <PageLayout>
+        <div className="flex items-center justify-center h-screen">
+          <Typography variant="h4" className="text-maltYellow">
+            {'Nema sponzora'}
+          </Typography>
+        </div>
+      </PageLayout>
+    );
+  }
 
   return (
     <PageLayout isLoading={isLoading}>
-      <div className="flex items-center justify-center h-screen">
-        <Text size="large" color="maltYellow" text={'Sponzori'} />
+      <div className="flex flex-col items-center px-20">
+        {sponsors?.map((sponsor, index) => (
+          <React.Fragment key={sponsor?.createdAt}>
+            {index !== 0 && <Separator className="flex w-full h-10 my-10" />}
+            <div className="flex flex-col justify-center border-[3px] overflow-hidden border-maltYellow rounded-3xl mt-5  bg-guinessBlack lg:w-[55%] w-full max-h-none hover:scale-105 hover:cursor-pointer transition-all duration-[500ms]">
+              <div className="flex gap-2 lg:flex-row flex-col ">
+                <div
+                  className={`flex min-w-[250px] lg:min-h-[250px] min-h-[300px] items-center overflow-hidden relative `}
+                >
+                  <img
+                    alt={`${sponsor?.name} logo`}
+                    src={sponsor?.logo}
+                    className="absolute min-w-[1000%] min-h-[1000%] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] scale-[0.1001]"
+                  />
+                </div>
+                <div className="flex flex-col items-center justify-between p-[20px] lg:w-[75%] w-full">
+                  <div className="flex flex-col items-center">
+                    <Text
+                      className="mb-[15px] text-center font-bold text-[35px]"
+                      size={'large'}
+                      color={'maltYellow'}
+                      text={sponsor?.name}
+                    />
+                    <Text className="text-center" size="medium" color="white" text={sponsor?.description} />
+                  </div>
+                  <div className="flex gap-[25px] mt-4">
+                    <div className="flex min-h-[3rem] items-center">
+                      <a href={sponsor.facebook} rel="noreferrer" target="_blank">
+                        <FacebookIcon className="w-10 h-10 cursor-pointer hover:w-12 hover:h-12 transition-all duration-[350ms]" />
+                      </a>
+                    </div>
+                    <div className="flex min-h-[3rem] items-center">
+                      <a href={sponsor.instagram} rel="noreferrer" target="_blank">
+                        <InstagramIcon className="w-10 h-10 cursor-pointer hover:w-12 hover:h-12 transition-all duration-[350ms]" />
+                      </a>
+                    </div>
+                    <div className="flex min-h-[3rem] items-center">
+                      <a href={`mailto:${sponsor.email}`} rel="noreferrer" target="_blank">
+                        <EmailIcon className="w-10 h-10 cursor-pointer hover:w-12 hover:h-12 transition-all duration-[350ms]" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </React.Fragment>
+        ))}
       </div>
     </PageLayout>
   );
