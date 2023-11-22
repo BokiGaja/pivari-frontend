2d7b90545fec0b2d0b00729f503619808f5d1779
Boza <bgajic93@gmail.com>
2023-11-22 12:19:11 +0100

    Added About us

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useGetCollection } from '../../services/api/hooks/useGetCollection';
+import Text from '../Text/Text';
+import Markdown from 'react-markdown';
+import MarkdownImage from '../Markdown/MarkdownImage';
+
+const AboutUs = () => {
+  const { data: aboutUsData, isLoading, error } = useGetCollection('about-us', 'sr', '*');
+  const aboutUs = aboutUsData?.data?.attributes;
+
+  if (isLoading || error) return null;
+
+  return (
+    <div className="lg:flex mt-[100px] lg:mt-0 flex-col items-center">
+      <Text size="large" color="maltYellow" className="text-4xl mb-4 text-center" text={'O nama'} />
+      <Markdown
+        className="flex flex-col justify-center lg:items-center text-center text-white lg:mt-0 mt-4 max-w-[700px]"
+        components={{ p: React.Fragment, img: MarkdownImage }}
+      >
+        {aboutUs.text}
+      </Markdown>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/components/Markdown/MarkdownImage.jsx
+++ src/components/Markdown/MarkdownImage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MarkdownImage = (props) => {
-  return <img {...props} style={{ maxHeight: 800 }} />;
+  return <img {...props} style={{ maxHeight: 600, maxWidth: 600, paddingTop: 20, paddingBottom: 20 }} />;
 };
 
 export default MarkdownImage;
