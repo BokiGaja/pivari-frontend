88afc5e6164ac6ec04c300336c57359a67ccd15b
Boza <bgajic93@gmail.com>
2023-11-27 13:17:32 +0100

    Updated back to members part

--- src/pages/SingleMemberPage.jsx
+++ src/pages/SingleMemberPage.jsx
@@ -18,13 +18,14 @@ import { Typography } from '@mui/material';
 const SingleMemberPage = () => {
   const navigate = useNavigate();
   const params = useParams();
-  const route = useLocation()
+  const route = useLocation();
+  const routeName = route.pathname?.split('/')[1];
 
   const {
     data: memberData,
     isLoading,
     isRefetching,
-  } = useGetCollection(route.pathname?.split('/')[1], 'sr', '*', {
+  } = useGetCollection(routeName, 'sr', '*', {
     'filters[name][$eq]': params?.name?.replaceAll('-', ' '),
   });
   const member = memberData?.data?.[0]?.attributes;
@@ -44,8 +45,7 @@ const SingleMemberPage = () => {
     <PageLayout isLoading={isLoading || isRefetching}>
       <article className="flex flex-col w-[80%] mx-auto lg:mt-0 mt-24 lg:px-20 px-0 relative">
         <div className="flex lg:flex-row flex-col items-center justify-center mb-[50px]">
-          <div
-            className="flex w-[200px] min-w-[200px] min-h-[200px] lg:mr-[50px] mb-[20px] items-center relative overflow-hidden">
+          <div className="flex w-[200px] min-w-[200px] min-h-[200px] lg:mr-[50px] mb-[20px] items-center relative overflow-hidden">
             <img
               className="absolute min-w-[1000%] min-h-[1000%] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] scale-[0.1001]"
               src={member?.logo?.data?.attributes?.url ? member?.logo?.data?.attributes?.url : pivariLogo}
@@ -59,40 +59,44 @@ const SingleMemberPage = () => {
               color={'maltYellow'}
               text={member?.name}
             />
-            {member?.phone && <div className="flex gap-2">
-              <PhoneIcon className="h-7 w-7 mt-[5px]"/>
-              <Text size={'large'} color={'maltYellow'} text={member?.phone}/>
-            </div>}
-            {member?.address && <div className="flex gap-2">
-              <LocationIcon className="h-7 w-7 mt-[5px]"/>
-              <Text size={'large'} color={'maltYellow'} text={member?.address}/>
-            </div>}
+            {member?.phone && (
+              <div className="flex gap-2">
+                <PhoneIcon className="h-7 w-7 mt-[5px]" />
+                <Text size={'large'} color={'maltYellow'} text={member?.phone} />
+              </div>
+            )}
+            {member?.address && (
+              <div className="flex gap-2">
+                <LocationIcon className="h-7 w-7 mt-[5px]" />
+                <Text size={'large'} color={'maltYellow'} text={member?.address} />
+              </div>
+            )}
             <div className="flex flex-row gap-4 min-h-14 justify-center items-center p-2 mt-8">
               {member?.website && (
                 <div className="flex justify-center items-center min-h-14 min-w-14">
                   <a href={member?.website} rel="noreferrer" target="_blank">
-                    <WorldIcon className="h-8 w-8 hover:scale-110 transition-all duration-[350ms]"/>
+                    <WorldIcon className="h-8 w-8 hover:scale-110 transition-all duration-[350ms]" />
                   </a>
                 </div>
               )}
               {member?.facebook && (
                 <div className="flex justify-center items-center min-h-14 min-w-14">
                   <a href={member?.facebook} rel="noreferrer" target="_blank">
-                    <FacebookIcon className="h-10 w-10 hover:scale-110 transition-all duration-[350ms]"/>
+                    <FacebookIcon className="h-10 w-10 hover:scale-110 transition-all duration-[350ms]" />
                   </a>
                 </div>
               )}
               {member?.instagram && (
                 <div className="flex justify-center items-center min-h-14 min-w-14">
                   <a href={member?.instagram} rel="noreferrer" target="_blank">
-                    <InstagramIcon className="h-10 w-10 hover:scale-110 transition-all duration-[350ms]"/>
+                    <InstagramIcon className="h-10 w-10 hover:scale-110 transition-all duration-[350ms]" />
                   </a>
                 </div>
               )}
               {member?.email && (
                 <div className="flex justify-center items-center min-h-14 min-w-14">
                   <a href={`mailto:${member.email}`} rel="noreferrer" target="_blank">
-                    <EmailIcon className="h-10 w-10 hover:scale-110 transition-all duration-[350ms]"/>
+                    <EmailIcon className="h-10 w-10 hover:scale-110 transition-all duration-[350ms]" />
                   </a>
                 </div>
               )}
@@ -100,20 +104,20 @@ const SingleMemberPage = () => {
           </div>
         </div>
         <div className="mb-[45px] w-[80%] mx-auto">
-          <Text className="text-bold text-center" size={'large'} color={'white'} text={member?.description}/>
+          <Text className="text-bold text-center" size={'large'} color={'white'} text={member?.description} />
         </div>
         <div className="w-[80%] mx-auto">
-          <Text className="text-center whitespace-pre-wrap " size={'medium'} color={'white'} text={member?.content}/>
+          <Text className="text-center whitespace-pre-wrap " size={'medium'} color={'white'} text={member?.content} />
         </div>
         <div className="absolute -bottom-[8rem] left-[8vw] hover:scale-110 transition-all duration-[350ms]">
           <button
             className="flex items-center font-crimson text-maltYellow tracking-[0.05rem] gap-2"
             onClick={() => {
-              navigate(`/members`);
+              navigate(`/${routeName}`);
             }}
           >
-            <LeftArrowIcon className="h-10 w-10"/>
-            Nazad ka članovima
+            <LeftArrowIcon className="h-10 w-10" />
+            {`Nazad ka ${routeName === 'members' ? 'članovima' : 'sponzorima'}`}
           </button>
         </div>
       </article>
