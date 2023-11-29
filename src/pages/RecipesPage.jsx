bd0b3c7ecfd05264b8c14d417c6d3d493f29da8f
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2023-11-29 10:56:42 +0100

    Recipe feature Author (#8)
    * added author to recipes
    
    * author styling
    
    * minor bug fixes and console.log delete

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useGetCollection } from '../services/api/hooks/useGetCollection';
 import PageLayout from '../components/layout/PageLayout';
-import RecipeListItem from '../components/Article/RecipeListItem/RecipeListItem';
+import RecipeListItem from '../components/Recipe/RecipeListItem/RecipeListItem';
 
 const RecipesPage = () => {
   const { data: recipesData } = useGetCollection('recipes', 'sr', '*');
   const recipes = recipesData?.data?.map((recipe) => recipe.attributes);
+
   return (
     <PageLayout isLoading={false}>
       <div className="flex flex-col items-center justify-start h-screen">
