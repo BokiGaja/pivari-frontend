2f7b149ebcc2a362850e9730b7df09ee622eb9fa
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2023-12-01 21:32:14 +0100

    mnor styling ajustments

--- src/components/Carousel/CarouselSlider.jsx
+++ src/components/Carousel/CarouselSlider.jsx
@@ -64,39 +64,46 @@ const CarouselSlider = ({ carouselData }) => {
   };
 
   return (
-    <div className="lg:w-[85%] w-full mx-auto mt-[50px] relative ">
-      {isActive ? (
-        <div
-          onClick={handleClickExit}
-          className="fixed w-screen lg:h-screen h-[125vh] top-0 left-0 bg-[#282c34bb] z-[9]"
-        >
-          <div className="absolute lg:w-[500px] w-[280px] top-[20%] left-[35%] z-10">
-            <img className="w-full" src={popUrl} alt={popAlt} />
-          </div>
-        </div>
-      ) : null}
-      <button
-        className="absolute top-[50%] -left-[5%] lg:flex hidden justify-center rounded-full border-2 bg-transparent border-hopGreen p-2 pr-[10px] pl-[5px] hover:border-maltYellow hover:bg-guinessBlack transition-all duration-200"
-        onClick={() => slider?.current?.slickPrev()}
-      >
-        <LeftArrow className="w-8 h-8" />
-      </button>
-      <Slider ref={slider} {...settings}>
-        {carouselData?.map((img) => {
-          return (
-            <div key={img.id} className="flex w-full justify-center hover:cursor-pointer p-5">
-              <img onClick={handleClickImg} className="w-[350px]" src={img.attributes.url} alt={img.attributes.name} />
+    <React.Fragment>
+      <div className=" lg:w-[85%] w-full mx-auto mt-[50px] relative ">
+        {isActive ? (
+          <div
+            onClick={handleClickExit}
+            className="fixed flex justify-center items-center w-screen lg:h-screen h-[125vh] top-0 left-0 bg-[#282c34bb] z-[9]"
+          >
+            <div className=" lg:w-[500px] w-[280px] z-10">
+              <img className="w-full" src={popUrl} alt={popAlt} />
             </div>
-          );
-        })}
-      </Slider>
-      <button
-        className="absolute top-[50%] -right-[5%] lg:flex hidden justify-center rounded-full border-2 bg-transparent border-hopGreen p-2 pl-[10px] pr-[5px] hover:border-maltYellow hover:bg-guinessBlack transition-all duration-200"
-        onClick={() => slider?.current?.slickNext()}
-      >
-        <RightArrow className="w-8 h-8" />
-      </button>
-    </div>
+          </div>
+        ) : null}
+        <button
+          className="absolute top-[50%] -left-[5%] lg:flex hidden justify-center rounded-full border-2 bg-transparent border-hopGreen p-2 pr-[10px] pl-[5px] hover:border-maltYellow hover:bg-guinessBlack transition-all duration-200"
+          onClick={() => slider?.current?.slickPrev()}
+        >
+          <LeftArrow className="w-8 h-8" />
+        </button>
+        <Slider ref={slider} {...settings}>
+          {carouselData?.map((img) => {
+            return (
+              <div key={img.id} className="flex w-full justify-center hover:cursor-pointer p-5">
+                <img
+                  onClick={handleClickImg}
+                  className="w-[350px]"
+                  src={img.attributes.url}
+                  alt={img.attributes.name}
+                />
+              </div>
+            );
+          })}
+        </Slider>
+        <button
+          className="absolute top-[50%] -right-[5%] lg:flex hidden justify-center rounded-full border-2 bg-transparent border-hopGreen p-2 pl-[10px] pr-[5px] hover:border-maltYellow hover:bg-guinessBlack transition-all duration-200"
+          onClick={() => slider?.current?.slickNext()}
+        >
+          <RightArrow className="w-8 h-8" />
+        </button>
+      </div>
+    </React.Fragment>
   );
 };
 
