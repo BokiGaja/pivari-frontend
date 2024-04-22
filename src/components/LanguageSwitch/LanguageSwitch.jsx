24fd3d5344ebb067f8ba923a49ee3955c3b0227c
Desimir Popović <110031217+dessa995@users.noreply.github.com>
2024-04-22 20:21:53 +0200

    dropdown fix (#18)
    * dropdown fix
    
    * position fix

--- src/components/LanguageSwitch/LanguageSwitch.jsx
+++ src/components/LanguageSwitch/LanguageSwitch.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 import { ReactComponent as SerbiaFlag } from '../../assets/svg/serbia-flag.svg';
 import { ReactComponent as UKFlag } from '../../assets/svg/uk-flag.svg';
 import i18next from 'i18next';
@@ -24,8 +25,7 @@ const localeFlags = {
   },
 };
 
-const LanguageSwitch = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+const LanguageSwitch = ({ isDropdownOpen, setIsDropdownOpen, toggleDropdownDelayed }) => {
   const ref = useRef(null);
   const [currentLang, setCurrentLang] = useAtom(localeLanguageAtom);
   const navigate = useNavigate();
@@ -52,32 +52,46 @@ const LanguageSwitch = () => {
 
   return (
     <div className="flex items-center justify-around mr-4 relative" ref={ref}>
-      <div className="flex w-[40px] h-[40px] cursor-pointer" onClick={() => toggleDropdown()}>
+      <div
+        className="flex w-[40px] h-[40px] cursor-pointer"
+        onClick={() => {
+          toggleDropdown();
+          toggleDropdownDelayed();
+        }}
+      >
         {localeFlags?.[currentLang]?.flag}
       </div>
-      {isDropdownOpen && (
-        <div
-          className="lg:absolute flex flex-1 flex-col top-16 right-0 bg-blackBackground border border-hopGreen p-2 shadow-md rounded-md overflow-hidden"
-          onClick={() => toggleDropdown()}
-        >
-          {Object.keys(localeFlags)?.map((localeFlagKey) => (
-            <div
-              className="flex flex-row items-center justify-start rounded-md cursor-pointer hover:bg-blackBackgroundLighter p-2"
-              key={localeFlags[localeFlagKey].name}
-              onClick={() => {
-                setCurrentLang(localeFlagKey);
-                i18next.changeLanguage(localeFlagKey);
-                navigate(ROUTES.HOME);
-              }}
-            >
-              <div className="flex w-[40px] h-[40px] cursor-pointer mr-2">{localeFlags[localeFlagKey].flag}</div>
-              <Text size="large" color="white" text={localeFlags[localeFlagKey].name} />
-            </div>
-          ))}
-        </div>
-      )}
+      {/* {isDropdownOpen && ( */}
+      <div
+        className={`absolute flex flex-1 flex-col top-16 lg:right-0 -right-18 bg-blackBackground border border-hopGreen p-2 shadow-md rounded-md overflow-hidden transition-all duration-300 ${
+          isDropdownOpen ? 'visible max-h-[250px]' : 'invisible max-h-0 !py-0'
+        }`}
+        onClick={() => toggleDropdown()}
+      >
+        {Object.keys(localeFlags)?.map((localeFlagKey) => (
+          <div
+            className="flex flex-row items-center justify-start rounded-md cursor-pointer hover:bg-blackBackgroundLighter p-2"
+            key={localeFlags[localeFlagKey].name}
+            onClick={() => {
+              setCurrentLang(localeFlagKey);
+              i18next.changeLanguage(localeFlagKey);
+              navigate(ROUTES.HOME);
+            }}
+          >
+            <div className="flex w-[40px] h-[40px] cursor-pointer mr-2">{localeFlags[localeFlagKey].flag}</div>
+            <Text size="large" color="white" text={localeFlags[localeFlagKey].name} />
+          </div>
+        ))}
+      </div>
+      {/* )} */}
     </div>
   );
+};
+
+LanguageSwitch.propTypes = {
+  isDropdownOpen: PropTypes.bool.isRequired,
+  setIsDropdownOpen: PropTypes.func.isRequired,
+  toggleDropdownDelayed: PropTypes.func.isRequired,
 };
 
 export default LanguageSwitch;
